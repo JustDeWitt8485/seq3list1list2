595227d4f33de87c329735578bb0e9f40edc5b51
Tracy DeWitt <Justdewitt8485@gmail.com>
2020-08-18 16:49:45 -0500

    Made changes to linear_merge (no sort method)

--- list2.py
+++ list2.py
@@ -85,7 +85,12 @@ def empty_filter(list1):
 
 def linear_merge(list1, list2):
     list1.extend(list2)
-    return sorted(list1)
+
+    for i in range(len(list1)):
+        for j in range(i + 1, len(list1)):
+            if list1[i] > list1[j]:
+                list1[i], list1[j] = list1[j], list1[i]
+    return list1
 
 
 # Provided simple test() function used in main() to print
